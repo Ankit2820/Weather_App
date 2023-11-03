641af9a39c23f3c929d49fffb01a9ec9e549417d
Ankit <ankit.abhishek125@gmail.com>
2023-11-04 04:30:29 +0530

    first commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "17fdd9d39af2cf030375f924dcd2c5ab",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
